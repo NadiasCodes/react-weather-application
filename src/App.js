b8139b1192270cd1b47e004747b3356c7d475bed
NadiasCodes <De.nadia@outlook.com>
2024-01-22 11:23:22 +0100

    Added the GitHub repo URL, my profile and vercel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/nadia-bouli/"
+          target="blank
+        "
+        >
+          Nadia Bouli
+        </a>{" "}
+        with{" "}
+        <a href="https://create-react-app.dev/" target="blank">
+          React
+        </a>
+        , is opened sourced on
+        <a
+          href="https://github.com/NadiasCodes/react-weather-application"
+          target="blank
+        "
+        >
+          GitHub
+        </a>{" "}
+        & hosted on{" "}
+        <a
+          href="https://weather-app-made-with-react.vercel.app/"
+          target="blank
+        "
+        >
+          Vercel
+        </a>
+      </footer>
+    </div>
+  );
 }
