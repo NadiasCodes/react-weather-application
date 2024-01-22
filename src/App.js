810cc19546cb045b1710cb3a8248b49478fc51c9
NadiasCodes <De.nadia@outlook.com>
2024-01-22 11:41:19 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,38 +3,40 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
+      <div className="container">
+        <h1>Weather app</h1>
 
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/nadia-bouli/"
-          target="blank
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/nadia-bouli/"
+            target="blank
         "
-        >
-          Nadia Bouli
-        </a>{" "}
-        with{" "}
-        <a href="https://create-react-app.dev/" target="blank">
-          React
-        </a>
-        , is opened sourced on
-        <a
-          href="https://github.com/NadiasCodes/react-weather-application"
-          target="blank
+          >
+            Nadia Bouli
+          </a>{" "}
+          with{" "}
+          <a href="https://create-react-app.dev/" target="blank">
+            React
+          </a>
+          , is opened sourced on
+          <a
+            href="https://github.com/NadiasCodes/react-weather-application"
+            target="blank
         "
-        >
-          GitHub
-        </a>{" "}
-        & hosted on{" "}
-        <a
-          href="https://weather-app-made-with-react.vercel.app/"
-          target="blank
+          >
+            GitHub
+          </a>{" "}
+          & hosted on{" "}
+          <a
+            href="https://weather-app-made-with-react.vercel.app/"
+            target="blank
         "
-        >
-          Vercel
-        </a>
-      </footer>
+          >
+            Vercel
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
